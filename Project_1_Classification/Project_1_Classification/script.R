0d89ff34ce2c7745fee7470a786d6c6727803f1a
shanealv <shane.alv@gmail.com>
2016-10-10 23:41:46 -0600

    Added more to script

--- Project_1_Classification/Project_1_Classification/script.R
+++ Project_1_Classification/Project_1_Classification/script.R
@@ -1,7 +1,20 @@
 # Load Libraries ---
 library("rpart")
-# ----
 
 # Load Data Sets ---
-mushdata <- read.table("Data\agaricus-lepiota.data", sep = ",");
-#---
+mushrooms.table <- read.table("Data/agaricus-lepiota.data", sep = ",");
+names(mushrooms.table) <- c("Class", "CS", "CC", "B", "O", "GA", "GSp", "GSi", "GC", "SS", "SR", "SSuAR", "SSuBr", "SCAR", "SCBR", "VT", "VC", "RN", "RT", "SPC", "P", "H");
+mushrooms.classlabel <- mushrooms.table[, 1];
+mushrooms.data <- mushrooms.table[, -1];
+set.seed(123);
+mushrooms.size <- nrow(mushrooms.table);
+mushrooms.train_index <- sample(seq_len(mushrooms.size), size = floor(0.75 * mushrooms.size));
+mushrooms.training <- mushrooms.table[mushrooms.train_index,]
+mushrooms.test <- mushrooms.table[ - mushrooms.train_index,]
+
+# Other Configuration ---
+options(max.print = 230);
+
+# Rpart on Mushroom Data ---
+mushrooms.dtModel <- rpart(Class ~ CS+CC+B+O+GA+GSp+GSi+GC+SS+SR+SSuAR+SSuBr+SCAR+SCBR+VT+VC+RN+RT+SPC+P+H, data = mushrooms.table);
+plot.rpart(mushrooms.dtModel);
