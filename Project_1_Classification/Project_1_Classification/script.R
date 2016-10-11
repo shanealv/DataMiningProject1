833e10980f30245cb24e4ecf305099ad41bc1315
shanealv <shane.alv@gmail.com>
2016-10-11 13:24:00 -0600

    Swithed to use party instead of rpart

--- Project_1_Classification/Project_1_Classification/script.R
+++ Project_1_Classification/Project_1_Classification/script.R
@@ -1,5 +1,7 @@
+# source("script.R")
 # Load Libraries ---
-library("rpart")
+install.packages("party", lib = "lib")
+library("party", lib.loc = "lib")
 
 # Load Data Sets ---
 mushrooms.table <- read.table("Data/agaricus-lepiota.data", sep = ",");
@@ -10,11 +12,11 @@ set.seed(123);
 mushrooms.size <- nrow(mushrooms.table);
 mushrooms.train_index <- sample(seq_len(mushrooms.size), size = floor(0.75 * mushrooms.size));
 mushrooms.training <- mushrooms.table[mushrooms.train_index,];
-mushrooms.test <- mushrooms.table[ - mushrooms.train_index,];
+mushrooms.testing <- mushrooms.table[ - mushrooms.train_index,];
 
 # Other Configuration ---
 options(max.print = 230);
 
 # Rpart on Mushroom Data ---
-mushrooms.dtModel <- rpart(Class ~ CS+CC+B+O+GA+GSp+GSi+GC+SS+SR+SSuAR+SSuBr+SCAR+SCBR+VT+VC+RN+RT+SPC+P+H, data = mushrooms.table);
-plot.rpart(mushrooms.dtModel);
+mushrooms.tree <- ctree(Class ~ CS + CC + B + O + GA + GSp + GSi + GC + SS + SR + SSuAR + SSuBr + SCAR + SCBR + VT + VC + RN + RT + SPC + P + H, data = mushrooms.table);
+plot(mushrooms.tree);
