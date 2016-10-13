c05562b00732b64f1ce16f82ad5c6f49e95fb864
shanealv <shane.alv@gmail.com>
2016-10-12 23:07:06 -0600

    Cleaned up code and added SVM

--- Project_1_Classification/Project_1_Classification/script.R
+++ Project_1_Classification/Project_1_Classification/script.R
@@ -1,58 +1,53 @@
 # source("script.R")
-# Load Libraries ---
-usePackage <- function(p) {
+# Utility Functions ----
+UsePackage <- function(p) {
     if (!is.element(p, installed.packages()[, 1]))
         install.packages(p)
-    library(p, character.only = TRUE)
+    require(p, character.only = TRUE)
 }
-usePackage("tree");
 
-# Load Data Sets ---
-mushrooms <- read.table("Data/agaricus-lepiota.data", sep = ",");
-names(mushrooms) <- c("Class", "CS", "CC", "B", "O", "GA", "GSp", "GSi", "GC", "SS", "SR", "SSuAR", "SSuBr", "SCAR", "SCBR", "VT", "VC", "RN", "RT", "SPC", "P", "H");
-mushrooms.class <- mushrooms[[1]]
-set.seed(123);
-mushrooms.size <- nrow(mushrooms);
-mushrooms.train_idx <- sample(seq_len(mushrooms.size), size = floor(0.75 * mushrooms.size));
-mushrooms.test_idx <- -mushrooms.train_idx;
-mushrooms.train <- mushrooms[mushrooms.train_idx,];
-mushrooms.test <- mushrooms[mushrooms.test_idx,];
-mushrooms.testclass <- mushrooms.class[mushrooms.test_idx];
+FindPRF <- function(actual, pred) {
+    # convert the values in the vectors binary 0 1
+    actual = sapply(actual, c) - 1;
+    predicted = sapply(pred, c) - 1;
+    precision = sum(predicted & actual) / (sum(predicted & actual) + sum(predicted & !actual));
+    recall = sum(predicted & actual) / (sum(predicted & actual) + sum(!predicted & actual));
+    fMeasure = 2 * precision * recall / (precision + recall);
+    cat("Precision:\t", precision, "\nRecall:\t\t", recall, "\nF1:\t\t\t", fMeasure, "\n");
+}
 
+CrossValidate <- function(test, k) {
+
+}
+
+# Load Libraries ---
+UsePackage("tree");
+UsePackage("e1071"); # used for svm
 
 # Other Configuration ---
 options(max.print = 230);
+# Load Data Sets ---
+mushrooms.table <- read.table("Data/agaricus-lepiota.data", sep = ",");
+mushrooms.size <- nrow(mushrooms.table);
+mushrooms <- data.frame(mushrooms.table);
 
-# Create tree for Mushroom Data ---
-mushrooms.tree <- tree(Class ~ CS + CC + B + O + GA + GSp + GSi + GC + SS + SR + SSuAR + SSuBr + SCAR + SCBR + VT + VC + RN + RT + SPC + P + H, data = mushrooms.train);
+# Create a Model
+set.seed(2079345);
+mushrooms.split <- sample(seq_len(mushrooms.size), size = floor(0.75 * mushrooms.size));
+mushrooms.train <- mushrooms[mushrooms.split,];
+mushrooms.test <- mushrooms[ - mushrooms.split,];
+
+set.seed(2079345);
+# Create tree for Mushroom Data and test it
+mushrooms.tree <- tree(V1 ~ ., data = mushrooms.train);
+mushrooms.tree.pred <- predict(mushrooms.tree, mushrooms.test, type = 'class');
+FindPRF(mushrooms.test[, 1], mushrooms.tree.pred);
+
+# Create a support vector machine for Mushroom Data and test it
+mushrooms.svm <- svm(V1 ~ ., data.frame(sapply(mushrooms.train, c)), scale = FALSE);
+mushrooms.svm.pred <- predict(mushrooms.svm, data.frame(sapply(mushrooms.test, c)), type = 'class');
+FindPRF(mushrooms.test[, 1], mushrooms.svm.pred);
 
 # Plot the tree
 plot(mushrooms.tree);
-text(mushrooms.tree, pretty = 0);
-
-# Check how the model is doing using the test data
-mushrooms.tree.pred <- predict(mushrooms.tree, mushrooms.test, type = 'class');
-
-PrintStats <- function(actual, pred) {
-    map <- mapply(list, actual, pred, SIMPLIFY = FALSE);
-    TP <- 0; TN <- 0; FP <- 0; FN <- 0;
-    for (pair in map) {
-        if (pair[1] == 2 && pair[2] == 2) {
-            TP <- TP +1;
-        }
-        if (pair[1] == 1 && pair[2] == 1) {
-            TN <- TN +1;
-        }
-        if (pair[1] == 1 && pair[2] == 2) {
-            FP <- FP +1;
-        }
-        if (pair[1] == 2 && pair[2] == 1) {
-            FN <- FN +1;
-        }
-    }
-    cat(TP, TN, FP, FN, sep = " ");
-}
-
-PrintStats(mushrooms.testclass, mushrooms.tree.pred);
-# Perform Cross Validation
-set.seed(123);
+text(mushrooms.tree);
