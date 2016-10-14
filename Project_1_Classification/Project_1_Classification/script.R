e32e0c751f29f45dc29828c9b60d47976360ddb3
shanealv <shane.alv@gmail.com>
2016-10-13 18:46:30 -0600

    Added untested cross validation

--- Project_1_Classification/Project_1_Classification/script.R
+++ Project_1_Classification/Project_1_Classification/script.R
@@ -13,16 +13,42 @@ FindPRF <- function(actual, pred) {
     precision = sum(predicted & actual) / (sum(predicted & actual) + sum(predicted & !actual));
     recall = sum(predicted & actual) / (sum(predicted & actual) + sum(!predicted & actual));
     fMeasure = 2 * precision * recall / (precision + recall);
-    cat("Precision:\t", precision, "\nRecall:\t\t", recall, "\nF1:\t\t\t", fMeasure, "\n");
+    return(c(precision, recall, fMeasure));
 }
 
-CrossValidate <- function(test, k) {
+PrintPRF <- function(prf) {
+    cat("Precision:\t", prf[1], "\nRecall:\t\t", prf[2], "\nF1:\t\t\t", prf[3], "\n");
+}
 
+CrossValidateTree <- function(formula, data) {
+    bins = rep(1:10, nrow(data) / 10 + 1)[1:nrow(data)];
+    #folds <- createFolds(mushrooms$V1, 10, FALSE)
+    segs = split(data, bins);
+    p <- 1:10;
+    r <- 1:10;
+    f <- 1:10;
+
+    for (index in 1:10) {
+        test <- segs[[index]];
+        all <- rbind(data, test);
+        train <- all[!duplicated(all, fromLast = FALSE) & !duplicated(all, fromLast = TRUE),];
+        tree <- tree(formula, data);
+        pred <- predict(tree, test, type = 'class');
+        prf <- FindPRF(test[[1]], pred);
+        p[index] = prf[1];
+        r[index] = prf[2];
+        f[index] = prf[3];
+    }
+
+    return(c(mean(p),mean(r),mean(f)));
 }
 
 # Load Libraries ---
 UsePackage("tree");
-UsePackage("e1071"); # used for svm
+# used for svm
+UsePackage("e1071");
+# used for createFolds
+UsePackage("caret");
 
 # Other Configuration ---
 options(max.print = 230);
@@ -39,14 +65,16 @@ mushrooms.test <- mushrooms[ - mushrooms.split,];
 
 set.seed(2079345);
 # Create tree for Mushroom Data and test it
-mushrooms.tree <- tree(V1 ~ ., data = mushrooms.train);
+mushrooms.tree <- tree(V1 ~ ., mushrooms.train);
 mushrooms.tree.pred <- predict(mushrooms.tree, mushrooms.test, type = 'class');
-FindPRF(mushrooms.test[, 1], mushrooms.tree.pred);
+mushrooms.tree.prf = FindPRF(mushrooms.test[, 1], mushrooms.tree.pred);
+PrintPRF(mushrooms.tree.prf);
 
 # Create a support vector machine for Mushroom Data and test it
 mushrooms.svm <- svm(V1 ~ ., data.frame(sapply(mushrooms.train, c)), scale = FALSE);
 mushrooms.svm.pred <- predict(mushrooms.svm, data.frame(sapply(mushrooms.test, c)), type = 'class');
-FindPRF(mushrooms.test[, 1], mushrooms.svm.pred);
+mushrooms.svm.prf = FindPRF(mushrooms.test[, 1], mushrooms.svm.pred);
+PrintPRF(mushrooms.svm.prf);
 
 # Plot the tree
 plot(mushrooms.tree);
