b8f53f1b3912ae45c927d6e7bddba7f501f5771e
shanealv <shane.alv@gmail.com>
2016-10-14 23:05:41 -0600

    Added some extra printing

--- Project_1_Classification/Project_1_Classification/script.R
+++ Project_1_Classification/Project_1_Classification/script.R
@@ -3,22 +3,28 @@ source("Scripts/Libraries.R");
 source("Scripts/Functions.R");
 source("Scripts/Data.R");
 
+cat("Mushroom Data\n");
 # Create tree for Mushroom Data and test it
+cat("tree\n");
 set.seed(2079345);
 TrainAndTest(as.factor(edible) ~ ., mushrooms, 0.75, 'tree');
 CrossValidate(as.factor(edible) ~ ., mushrooms, 'tree');
 
 # Create a support vector machine for Mushroom Data and test it
+cat("svm\n");
 set.seed(2079345);
 TrainAndTest(as.factor(edible) ~ ., mushrooms.simplified, 0.75, 'svm');
 CrossValidate(as.factor(edible) ~ ., mushrooms.simplified, 'svm');
 
+cat("Yeast Data\n");
 # Create tree for Yeast Data and test it
+cat("tree\n");
 set.seed(2079345);
 TrainAndTest(as.factor(ME) ~ ., yeast, 0.75, 'tree');
 CrossValidate(as.factor(ME) ~ ., yeast, 'tree');
 
 # Create a support vector machine for Yeast Data and test it
+cat("svm\n");
 set.seed(2079345);
 TrainAndTest(as.factor(ME) ~ ., yeast.simplified, 0.75, 'svm');
 CrossValidate(as.factor(ME) ~ ., yeast.simplified, 'svm');
